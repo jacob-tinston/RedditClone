8edd9791c849db37ddec2e5ef533bdd34f93abad
jacob-tinston <jtinston678@gmail.com>
2021-04-26 20:07:09 +0100

    £Added search handlers

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { search, selectSearchTerm } from '../features/searchBarSlice';
+import store from '../App/store'
 
 const SearchBar = () => {
+    const searchTerm = useSelector(selectSearchTerm)
+    const handleChange = ({ target }) => {
+        store.dispatch(search(target.value))
+    }
+
+    const fetchData = async (e) => {
+        e.preventDefault();
+        const data = await fetch(`https://www.reddit.com/r/${searchTerm}.json`);
+        const response = await data.json();
+        console.log(response.data.children);
+    }
+
     return (
         <div>
-            <form>
-                <input type="search"></input>
+            <form onSubmit={fetchData}>
+                <input type="search" onChange={handleChange}></input>
                 <button type="submit">Submit</button>
             </form>
         </div>
