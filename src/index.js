6a393aed213a9e08a78b622080cb6a96c4ac17f4
jacob-tinston <jtinston678@gmail.com>
2021-04-26 18:44:39 +0100

    Added provider

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
 import App from './App/App';
+import { store } from './App/store'
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
