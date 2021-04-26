7caf655831dd548c9ec1d7068200b4392e7217c3
jacob-tinston <jtinston678@gmail.com>
2021-04-26 20:05:09 +0100

    Imported store

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
 import App from './App/App';
-import { store } from './App/store'
+import store from './App/store'
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
