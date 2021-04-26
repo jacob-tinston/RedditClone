262fb62783814218871b1d5ddd1cc80baa054c48
jacob-tinston <jtinston678@gmail.com>
2021-04-26 17:47:44 +0100

    Added favicon, searchbar component

--- src/App/App.js
+++ src/App/App.js
@@ -1,8 +1,11 @@
 import './App.css';
+import SearchBar from '../components/searchBar';
 
 function App() {
   return (
     <div className="App">
+      <h1>Reddit Client</h1>
+      <SearchBar />
     </div>
   );
 }
---
+++ src/components/searchBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const SearchBar = () => {
+    return (
+        <div>
+            <form>
+                <input type="search"></input>
+                <button type="submit">Submit</button>
+            </form>
+        </div>
+    )
+};
+
+export default SearchBar;
