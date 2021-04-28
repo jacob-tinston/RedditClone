c0974b9dfa760914625d2b7784f7df9f66d0701b
jacob-tinston <jtinston678@gmail.com>
2021-04-28 17:21:32 +0100

    Added search results reducer

--- src/App/store.js
+++ src/App/store.js
@@ -1,10 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { searchBarSlice } from '../features/searchBarSlice';
+import { searchResultsSlice } from '../features/searchResultsSlice';
 
 const searchBarReducer = searchBarSlice.reducer;
+const searchResultsReducer = searchResultsSlice.reducer;
 
 export default configureStore({
     reducer: {
-      searchTerm: searchBarReducer
+      searchTerm: searchBarReducer,
+      searchResults: searchResultsReducer
     },
   });
