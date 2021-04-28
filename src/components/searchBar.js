2ea793f4f7ea96d2a4c917afec8279f7aa198f97
jacob-tinston <jtinston678@gmail.com>
2021-04-28 18:34:21 +0100

    Added searchTerm results to searchResults state

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { search, selectSearchTerm } from '../features/searchBarSlice';
+import { addSearchResults } from '../features/searchResultsSlice';
 import store from '../App/store'
 
 const SearchBar = () => {
     const searchTerm = useSelector(selectSearchTerm)
+
     const handleChange = ({ target }) => {
         store.dispatch(search(target.value))
     }
@@ -13,8 +15,11 @@ const SearchBar = () => {
         e.preventDefault();
         try {
             const data = await fetch(`https://www.reddit.com/r/${searchTerm}.json`);
-            const response = await data.json();
-            console.log(response.data.children);
+            const json = await data.json();
+            const response = await json.data.children;
+            for(let key = 0; key <= response.length - 1; key++) {
+                store.dispatch(addSearchResults(response[key].data));
+            }
         } catch(err) {
             window.alert(`Sorry! Could not find results for '${searchTerm}', please enter a valid search term...`);
             console.log(err);
