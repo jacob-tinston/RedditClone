9affd2563126a467cf0dc1e96822203eb658106c
jacob-tinston <jtinston678@gmail.com>
2021-04-28 18:56:32 +0100

    Added postlist component

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import SearchBar from '../components/searchBar';
+import PostList from '../components/postList';
 
 function App() {
   return (
     <div className="App">
       <h1>Reddit Client</h1>
       <SearchBar />
+      <PostList />
     </div>
   );
 }
